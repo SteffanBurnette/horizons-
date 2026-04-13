84d3e8634cfd0d68d7e328c03f2a0050a3a44ce6
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-04-13 18:28:28 -0400

    test

---
+++ data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        name: "waitomo Glowworm Caves",
+        location: "Waitomo",
+        country: "New Zealand",
+        continent: "Oceania",
+        is_open_to_public: true,
+        details: [
+            {
+                fun_facts: 
+                "The glowworms create a star-like effect on the cave ceiling using bioluminescence."
+            },
+            {
+                description:
+                   "A subterranean network of limestone caverns famous for its magical boat rides under twinkling glowworm ceilings."
+            },
+        ],
+        uuid: "550e8400-e29b-41d4-a716-446655440001",
+    },
+    {
+        name: "The Door to Hell",
+        location: "Darvaza",
+        country: "Turkmenistan",
+        continent: "Asia",
+        is_open_to_public: true,
+        details: [
+            {
+                fun_facts: 
+                "This fiery crater has been burning since 1971 after a drilling incident"
+            },
+            {
+                description:
+                   "A subterranean network of limestone caverns famous for its magical boat rides under twinkling glowworm ceilings."
+            },
+        ],
+        uuid: "660e8400-e29b-41d4-a716-446655440001",
+    }
+]
